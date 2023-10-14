1b376b842822f3fd1ddaeb9e80285e2da58e6506
paulohBarbosa <paulobarbosa@hotmail.com>
2023-10-14 09:35:36 -0300

    Aula 01

---
+++ src/main/java/br/com/paulobarbosa/todoLists/TodoListsApplication.java
@@ -0,0 +1,13 @@
+package br.com.paulobarbosa.todoLists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/paulobarbosa/todoLists/TodoListsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulobarbosa.todoLists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
